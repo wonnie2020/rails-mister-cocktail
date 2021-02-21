adbe0e2fc3829d86cc7a095aae723d1c140a1ebf
Won Lee <cathykw2018@gmail.com>
2021-02-21 21:50:14 +0000

    more back end features

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,23 @@
 class DosesController < ApplicationController
+def new
+  @cocktail = Cocktail.find(params[:cocktail_id])
+  @dose = Dose.new
+end
+
+# def create
+#     @ = Cocktail.new(cocktail_params)
+#     if @cocktail.save
+#       redirect_to cocktail_path(@cocktail)
+#     else
+#       render 'new'
+#     end
+#   end
+
+#   private
+
+#   def cocktail_params
+#     params.require(:cocktail).permit(:name)
+#   end
+# end
+
 end
